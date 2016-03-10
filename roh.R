0bfdefdb8604bcc341bc6f88e482bc9fe616df32
David McGaughey <mcgaughey@gmail.com>
2016-03-10 15:58:48 -0500

    does roh plots for a single person

--- roh.R
+++ roh.R
@@ -14,7 +14,17 @@ ggplot(CCGO59,aes(x=Pos,y=HomozygousState)) + geom_point() + facet_wrap(~Chr,sca
 runs <- rle2(CCGO59$HomozygousState,indices = TRUE)
 runs <- data.table(runs)
 nrow(runs %>% dplyr::filter(((stops-starts) > 50) & values==1) )
-head(runs %>% dplyr::filter(((stops-starts) > 50) & values==1) %>% arrange(-lengths))
+# find roh block with >50 consecutive calls of homozygosity
+over50 <- (runs %>% dplyr::filter(((stops-starts) > 50) & values==1) %>% arrange(-lengths))
+# calculate genomic space in the roh
+over50$GenomicDistance<-apply(over50,1,function(x) abs(CCGO59[x[2]]$Pos - CCGO59[x[3]]$Pos))
+# only keep roh with >1,000,000kb
+over50 <- over50 %>% filter(GenomicDistance > 1000000)
 
-#CCGO59$ROH <- ''
+# label original DF with roh blocks
+CCGO59$ROH <- "No"
+for (i in 1:nrow(over50)){
+  CCGO59[seq(over50[i,]$starts,over50[i,]$stops),"ROH"] <- "Yes"
+}
 
+ggplot(CCGO59,aes(x=Pos,y=HomozygousState,colour=ROH)) + geom_point() + facet_wrap(~Chr,scales='free_x',ncol=2)
