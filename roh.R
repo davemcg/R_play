124b00089ce0376270c180d369958b252ce2158f
David McGaughey <mcgaughey@gmail.com>
2016-03-11 09:01:29 -0500

    kind of works now. will plot all data, roh segments, and hand annotated variants can be plotted

--- roh.R
+++ roh.R
@@ -41,5 +41,12 @@ test$Chr <- factor(test$Chr,levels=c("chr1","chr2","chr3","chr4","chr5","chr6","
 
 ggplot() + geom_point(data=test,aes(x=Pos,y=ROH)) + facet_wrap(~Chr,ncol=2)
 
+var <- cbind(c("chr12","chr7","chr1","chr15"),c(9465464,65548068,153314125,72190416),c(0,0,0,0),c('..','..','..','..'),c(62,62,62,62),c("RP11-22B23.1","ASL","PGLYRP4","MYO9A"))
+colnames(var) <- c("Chr","Pos","HomozygousState","Quality","ROH","Gene")
+var <- data.table(var)
+var$Chr <- factor(var$Chr,levels=c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"))
+var$Pos <- as.numeric(var$Pos)
 
-ggplot(merged.data.frame) + geom_point(aes(x=Pos,y=ROH.1)) + geom_point(aes(x=Pos,y=ROH.2)) + geom_point(aes(x=Pos,y=ROH.3)) + geom_point(aes(x=Pos,y=ROH.4)) + facet_wrap(~Chr,scales='free_x',ncol=2)
+test$Gene <- ''
+test2<-rbind(test,var)
+test3 <- test2 %>% dplyr::filter(ROH != "No")
