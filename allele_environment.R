ea3a58bac47181e4a285271500874adb9bc2aeb4
David McGaughey <mcgaughey@gmail.com>
2016-04-21 17:24:04 -0400

    now with little function to count adjacent alleles in a user-defined window

--- allele_environment.R
+++ allele_environment.R
@@ -6,3 +6,16 @@ chr3<-data.frame(chr3)
 colnames(chr3) <- c("Chr","Position","ID","Ref","Alt","Qual","Filter","Info")
 chr3$AF <- apply(chr3,1,function(x) strsplit(x[8],";")[[1]][12])
 chr3 <- chr3[,c(1,2,3,4,5,6,7,9)]
+chr3 <- chr3[!duplicated(chr3),]
+chr3 <- data.table(chr3)
+# chr3[Position<(239313+250) & Position>(239313-250),]
+
+countAdjacentAlleles <- function(chr_data_table, window_size) {
+  chr_data_table <- data.table(chr_data_table)
+  out <- sapply(chr_data_table[,Position],function(x) 
+          nrow( 
+            chr_data_table[Position < (x + window_size) & 
+                           Position > (x - window_size),])
+          )
+  return(out)
+  }
