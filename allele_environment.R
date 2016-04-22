99c1a2675cd258722f50ef664bd36507276d06c5
David McGaughey <mcgaughey@gmail.com>
2016-04-22 09:46:48 -0400

    OK, having to get very core to have any speed

--- allele_environment.R
+++ allele_environment.R
@@ -11,11 +11,66 @@ chr3 <- data.table(chr3)
 # chr3[Position<(239313+250) & Position>(239313-250),]
 
 countAdjacentAlleles <- function(chr_data_table, window_size) {
-  chr_data_table <- data.table(chr_data_table)
+  # too damn slow. About 8s to do the first 10,000 with 250bp window in chr3
+  #chr_data_table <- data.table(chr_data_table)
   out <- sapply(chr_data_table[,Position],function(x) 
-          nrow( 
-            chr_data_table[Position < (x + window_size) & 
-                           Position > (x - window_size),])
-          )
+    nrow( 
+      chr_data_table[Position < (x + window_size) & 
+                       Position > (x - window_size),])
+  )
   return(out)
+}
+
+countAdjacentAlleles2 <- function(chr_vcf, window_size) {
+  # a bit faster
+  chr_vcf <- data.frame(chr_vcf)
+  out <- sapply(chr_vcf[,'Position'],function(x) 
+    nrow( 
+      subset(chr_vcf, Position < (x + window_size) & 
+                       Position > (x - window_size))
+      )
+  )
+  return(out)
+}
+
+countAdjacentAlleles3 <- function(chr_vcf, window_size) {
+  # a bit faster
+  chr_vcf <- data.table(chr_vcf)
+  positions <- as.integer(chr_vcf$Position)
+  out <- vector(mode="integer",length=nrow(chr_vcf))
+  out <- sapply(positions,function(x) 
+    nrow( 
+      chr_vcf %>% filter(Position < (x + window_size) & 
+                           Position > (x - window_size))
+    )
+  )
+  return(out)
+}
+
+total <- vector(mode='integer',length=length(positions))
+system.time(
+for (i in 1:length(positions)) {
+  counter <- 1
+  while ((i+counter)<length(positions) & 
+         abs(positions[i]-positions[i+counter])<250) {
+    counter=counter+1
   }
+  total[i] <- counter - 1
+} )
+
+total_down <- vector(mode='integer',length=length(positions))
+system.time(
+for (i in length(positions):1) {
+  counter <- 0
+  while (
+    abs(positions[i]-positions[i-counter])<250 ) {
+    counter=counter+1
+    if (i-counter<1) break #kills while loop when hitting the end
+  }
+  total_down[i] <- counter - 1
+} )
+
+
+# genomicRanges?
+library(VariantAnnotation)
+exac <- readVcf("~/Desktop/exac/ExAC_nonTCGA.r0.3.1.sites.vep.PASSonly.vcf.gz","GRCh37")
